9982ebc27d9fa558d4c550f3cb436f6b403dffa6
Cory Weller <cory.a.weller@gmail.com>
2026-07-20 16:41:38 -0400

    remove file

--- cellbender.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-$(echo $LOADEDMODULES | grep singularity >& /dev/null) || ml singularity >& /dev/null
-
-. /usr/local/current/singularity/app_conf/sing_binds
-
-export SINGULARITY_BINDPATH="${SINGULARITY_BINDPATH},/usr/local/apps"
-
-singularity exec --nv "$(dirname $(readlink -f ${BASH_SOURCE[0]}))/cellbender-0.3.2.sif" "$(basename $0)" "$@"
